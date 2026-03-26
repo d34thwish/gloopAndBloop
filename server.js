497623796db5e51dfb210d1b7b03a3a0bae44f9a
Ades <91234504+d34thwish@users.noreply.github.com>
2026-03-26 21:15:18 +0000

    Handle websocket connect and disconnect

--- server.js
+++ server.js
@@ -3,3 +3,10 @@ const WebSocket = require("ws");
 const wss = new WebSocket.Server({port: 8080});
 
 console.log("Server running");
+
+wss.on("connection", (ws) => {
+    console.log("Someone connected");
+    ws.on("close", (ws) => {
+        console.log("Someone disconnected");
+    });
+});
