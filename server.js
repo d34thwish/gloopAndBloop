adb432e0e0c304cf13dce21a588f723fc3714df5
Ades <91234504+d34thwish@users.noreply.github.com>
2026-03-26 21:38:31 +0000

    Added collection to save clients

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const WebSocket = require("ws");
+const clients = new Map();
 
 const wss = new WebSocket.Server({port: 8080});
 
@@ -9,8 +10,12 @@ console.log("Server running");
 wss.on("connection", (ws) => {
     ws.id = id++;
     console.log(`User ${ws.id} connected`);
+    clients.set(ws.id, ws);
+
+    console.log("Clients:", clients.size);
 
     ws.on("close", () => {
         console.log(`User ${ws.id} disconnected`);
+        clients.delete(ws.id);
     });
 });
